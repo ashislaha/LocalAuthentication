76b17c0ba702011cf3037c4aad2fd761db9de371
Ashis Laha <ashis.laha@walmartlabs.com>
2018-11-14 09:39:12 +0530

    adding home and log in VC

--- LocalAuthentication/LocalAuthentication/HomeViewController.swift
+++ LocalAuthentication/LocalAuthentication/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  LocalAuthentication
 //
 //  Created by Ashis Laha on 11/14/18.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .green
     }
-
-
 }
-
